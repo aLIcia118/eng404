8fbe653be98223534f4ed75f6d55f9fc9ddec188
Ruimeng Chen <rc4889@nyu.edu>
2025-10-07 19:03:06 -0400

    Be the same stage with main 2

--- cities/tests/test_queries.py
+++ cities/tests/test_queries.py
@@ -2,18 +2,6 @@ import pytest
 
 import cities.queries as qry
 
-
-def test_good_create():
-    old_rec_count = len(qry.cities)  # let's correct this!
-    assert qry.create(qry.SAMPLE_CITY) >= 1
-    assert len(qry.cities) > old_rec_count
-
-
-def test_create_bad_name():
-    with pytest.raises(ValueError):
-        qry.create({})
-
-
-def test_create_bad_param_type():
+def test_create():
     with pytest.raises(ValueError):
         qry.create(17)

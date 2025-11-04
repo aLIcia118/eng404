0e7a8ae454c74e1dcab82d35d9571aca015da882
JennyDonghe <hd2365@nyu.edu>
2025-11-04 16:03:27 -0500

    eddited flask app end points

--- server/endpoints.py
+++ server/endpoints.py
@@ -2,9 +2,7 @@
 This is the file containing all of the endpoints for our flask app.
 The endpoint called `endpoints` will return all available endpoints.
 """
-
-from http import HTTPStatus
-import logging
+# from http import HTTPStatus
 
 from flask import Flask  # , request
 from flask_restx import Resource, Api  # , fields  # Namespace
@@ -18,12 +16,6 @@ app = Flask(__name__)
 CORS(app)
 api = Api(app)
 
-logging.basicConfig(
-    level=logging.INFO,  # set to DEBUG for more detailed output
-    format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
-)
-log = logging.getLogger(__name__)
-
 ERROR = 'Error'
 MESSAGE = 'Message'
 NUM_RECS = 'Number of Records'
@@ -31,29 +23,31 @@ READ = 'read'
 
 ENDPOINT_EP = '/endpoints'
 ENDPOINT_RESP = 'Available endpoints'
+
 HELLO_EP = '/hello'
 HELLO_RESP = 'hello'
-MESSAGE = 'Message'
 
 CITIES_EPS = '/cities'
 CITY_RESP = 'Cities'
-ENPOINT_RESP= 'Available endpoints'
+
 
 @api.route(f'{CITIES_EPS}/{READ}')
 class Cities(Resource):
+    """
+    The purpose of the HelloWorld class is to have a simple test to see if the
+    app is working at all.
+    """
     def get(self):
-        try: 
+        """
+        A trivial endpoint to see if the server is running.
+        """
+        try:
             cities = cqry.read()
             num_recs = len(cities)
-            log.info("Successfully fetched %d cities", num_recs)
-            # success → 200
-            return {CITY_RESP: cities, NUM_RECS: num_recs}, HTTPStatus.OK
         except ConnectionError as e:
-            log.warning("Connection error in /cities/read: %s", e)
-            # backend unavailable → 503
-            return {ERROR: str(e)}, HTTPStatus.SERVICE_UNAVAILABLE
-        print(f'{cities=}')
-        return{
+            return {ERROR: str(e)}
+        
+        return {
             CITY_RESP: cities,
             NUM_RECS: num_recs,
         }
@@ -69,8 +63,7 @@ class HelloWorld(Resource):
         """
         A trivial endpoint to see if the server is running.
         """
-        log.debug("Received request on /hello")
-        return {HELLO_RESP: 'world'}, HTTPStatus.OK
+        return {HELLO_RESP: 'world'}
 
 
 @api.route(ENDPOINT_EP)
@@ -84,5 +77,4 @@ class Endpoints(Resource):
         The `get()` method will return a sorted list of available endpoints.
         """
         endpoints = sorted(rule.rule for rule in api.app.url_map.iter_rules())
-        log.info("Listing %d endpoints", len(endpoints))
-        return {"Available endpoints": endpoints}, HTTPStatus.OK
+        return {"Available endpoints": endpoints}

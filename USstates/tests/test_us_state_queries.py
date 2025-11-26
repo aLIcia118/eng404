60cc831cee17270f0c8b3aa6ab2ff1ce50bc2ff9
JennyDonghe <hd2365@nyu.edu>
2025-11-25 22:48:40 -0500

    added test queries

--- USstates/tests/test_us_state_queries.py
+++ USstates/tests/test_us_state_queries.py
@@ -1,18 +1,84 @@
+from copy import deepcopy
+
+from unittest.mock import patch
 import pytest
-import USstates.queries as qry
+
+import states.queries as qry
 
 
-def test_create_state():
-    count = qry.num_states()
-    qry.create(qry.SAMPLE_STATE)
-    assert qry.num_states() > count
+def get_temp_rec():
+    return deepcopy(qry.SAMPLE_STATE)
 
 
-def test_invalid_state_code():
+@pytest.fixture(scope='function')
+def temp_state_no_del():
+    temp_rec = get_temp_rec()
+    qry.create(get_temp_rec())
+    return temp_rec
+
+
+@pytest.fixture(scope='function')
+def temp_state():
+    temp_rec = get_temp_rec()
+    new_rec_id = qry.create(get_temp_rec())
+    yield new_rec_id
+    try:
+        qry.delete(temp_rec[qry.CODE], temp_rec[qry.COUNTRY_CODE])
+    except ValueError:
+        print('The record was already deleted.')
+
+
+@pytest.mark.skip('This is an example of a bad test!')
+def test_bad_test_for_count():
+    assert qry.count() == len(qry.cache)
+
+
+def test_count():
+    # get the count
+    old_count = qry.count()
+    # add a record
+    qry.create(get_temp_rec())
+    assert qry.count() == old_count + 1
+    qry.delete(qry.SAMPLE_CODE, qry.SAMPLE_COUNTRY)
+
+
+def test_good_create():
+    old_count = qry.count()
+    new_rec_id = qry.create(get_temp_rec())
+    assert qry.is_valid_id(new_rec_id)
+    assert qry.count() == old_count + 1
+    qry.delete(qry.SAMPLE_CODE, qry.SAMPLE_COUNTRY)
+
+
+def test_create_dup_key():
+    qry.create(get_temp_rec())
     with pytest.raises(ValueError):
-        qry.create({"name": "InvalidState", "code": "XYZ"})
+        qry.create(get_temp_rec())
+    qry.delete(qry.SAMPLE_CODE, qry.SAMPLE_COUNTRY)
 
 
-def test_create_non_dict():
+def test_create_bad_name():
     with pytest.raises(ValueError):
-        qry.create("CA")
+        qry.create({})
+
+
+def test_create_bad_param_type():
+    with pytest.raises(ValueError):
+        qry.create(17)
+
+
+def test_delete(temp_state_no_del):
+    ret = qry.delete(temp_state_no_del[qry.CODE],
+                     temp_state_no_del[qry.COUNTRY_CODE])
+    assert ret == 1
+
+
+def test_delete_not_there():
+    with pytest.raises(ValueError):
+        qry.delete('some state code that is not there', 'not a country code')
+
+
+def test_read(temp_state):
+    states = qry.read()
+    assert isinstance(states, dict)
+    assert qry.SAMPLE_KEY in states

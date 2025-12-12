4f0020249a9b8249494987c3314aacd67b1997e3
js12848 <js12848@nyu.edu>
2025-12-11 21:34:09 -0500

    Improve local.sh Flask startup script

--- local.sh
+++ local.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
 export FLASK_ENV=development
-export PROJ_DIR=$PWD
+# export PROJ_DIR=$PWD
+export PROJ_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 export DEBUG=1
 
 # run our server locally:
-PYTHONPATH=$(pwd):$PYTHONPATH
-FLASK_APP=server.endpoints flask run --debug --host=127.0.0.1 --port=8000
+# PYTHONPATH=$(pwd):$PYTHONPATH
+# Directory of this script = project root
+PYTHONPATH="$PROJ_DIR:${PYTHONPATH:-}"
+
+export FLASK_APP=server.endpoints
+
+flask run --debug --host=127.0.0.1 --port=8000
+
+# FLASK_APP=server.endpoints flask run --debug --host=127.0.0.1 --port=8000

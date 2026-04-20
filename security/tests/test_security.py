5af8147d65aa268dba434c957a4305375d458461
Ruimeng Chen <rc4889@nyu.edu>
2026-04-20 00:19:59 -0400

    test: cover security feature lookups

--- security/tests/test_security.py
+++ security/tests/test_security.py
@@ -7,3 +7,13 @@ def test_read():
     for feature in recs:
         assert isinstance(feature, str)
         assert len(feature) > 0
+
+def test_read_feature_returns_people_rules():
+    feature = sec.read_feature(sec.PEOPLE)
+    assert feature is not None
+    assert sec.CREATE in feature
+    assert feature[sec.CREATE][sec.CHECKS][sec.LOGIN] is True
+
+
+def test_read_feature_returns_none_for_unknown_feature():
+    assert sec.read_feature("unknown_feature") is None
